8cbe39b34e60a3ab6cdecf0b1950a9fbc02cb441
sara <sara.ahumidi4@gmail.com>
2023-05-10 12:08:20 +0300

    Initial Commit

---
+++ Wejha/WejhaApp.swift
@@ -0,0 +1,17 @@
+//
+//  WejhaApp.swift
+//  Wejha
+//
+//  Created by Sara Alhumidi on 20/10/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct WejhaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
